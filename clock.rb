9db4adfa2630c5c3cc9eb5a79416e1a4e61c9d18
felbalart <felbalart@gmail.com>
2017-05-07 06:05:51 -0300

    feat(clock): adds clockwork for scheduling

--- clock.rb
+++ clock.rb
@@ -2,7 +2,7 @@ require 'clockwork'
 require_relative 'program'
 
 module Clockwork
-  every(10.minute, 'depa.crawl') do
+  every(1.hour, 'depa.crawl') do
     DepaCrawler::Program.new.run
   end
 end
